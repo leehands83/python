75845a28b6e737c59f5ff99b98f273a7db08281a
nhlee_m1 <leehands83@icloud.com>
2021-05-02 18:35:30 +0900

    add bitcoin.py

--- tutorial/pyqt/bitcoin.py
+++ tutorial/pyqt/bitcoin.py
@@ -10,10 +10,10 @@ form_class = uic.loadUiType("bitcoin.ui")[0] # loadUiType 매서드는 QT Design
 
 class MyWindow(QMainWindow,form_class):
     def __init__(self):
-        super().__init__() # super() 는 파이썬의 내장함수
+        super().__init__() # super() 는 파이썬의 내장함수ㄱ
         self.setupUi(self) # form class 에 정의된 매서드로 QT Designer에서 만든 클래스들
         self.timer = QTimer(self)
-        self.timer.start(1000)
+        self.timer.start(100)
         self.timer.timeout.connect(self.timeout)
         self.pushButton.clicked.connect(self.inquiry)
 
@@ -29,9 +29,24 @@ class MyWindow(QMainWindow,form_class):
         self.statusBar().showMessage(str_time)
 
         price = pykorbit.get_current_price("BTC")
-        self.lineEdit.setText(str(price))
+        th_price = str(int(price/1000000))
+        hu_price = str(int((price/1000 )% 1000))
+        no_price = str(int(price%1000))
+
+        hu_price = hu_price.zfill(3)
+        no_price = no_price.zfill(3)
+
+        self.lineEdit.setText(th_price+","+hu_price+","+no_price)
+
         price = pykorbit.get_current_price("ETH")
-        self.lineEdit_ETH.setText(str(price))
+        th_price = str(int(price/1000000))
+        hu_price = str(int((price/1000 )% 1000))
+        no_price = str(int(price%1000))
+
+        hu_price = hu_price.zfill(3)
+        no_price = no_price.zfill(3)
+
+        self.lineEdit_ETH.setText(th_price+","+hu_price+","+no_price)
 
 
 app = QApplication(sys.argv)
